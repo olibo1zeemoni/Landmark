e0593d3374ec842e9378cabd2acfccbcdd3671ac
George Boahene <olibo1zeemoni13@gmail.com>
2023-09-01 07:53:36 +0000

    PageControl

--- Landmarks/Views/PageView/PageControl.swift
+++ Landmarks/Views/PageView/PageControl.swift
@@ -13,10 +13,14 @@ struct PageControl: UIViewRepresentable {
     var numberOfPages: Int
     @Binding var currentPage: Int
     
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
     func makeUIView(context: Context) -> UIPageControl {
         let control = UIPageControl()
         control.numberOfPages = numberOfPages
-        
+        control.addTarget(context.coordinator, action: #selector(Coordinator.updateCurrentPage(sender:)), for: .valueChanged)
         
         return control
     }
@@ -27,5 +31,17 @@ struct PageControl: UIViewRepresentable {
         uiView.currentPage = currentPage
     }
     
+    
+    class Coordinator: NSObject {
+        var control: PageControl
+        
+        init(_ control: PageControl) {
+            self.control = control
+        }
+        
+        @objc func updateCurrentPage(sender: UIPageControl) {
+            control.currentPage = sender.currentPage
+        }
+    }
     
 }
--- Landmarks/Views/PageView/PageView.swift
+++ Landmarks/Views/PageView/PageView.swift
@@ -14,52 +14,14 @@ struct PageView<Page: View> : View {
      
     var body: some View {
         ZStack(alignment: .bottomTrailing) {
-            VStack {
                 PageViewController(pages: pages, currentPage: $currentPage, direction: $direction)
                     
-                Text("Currrent Page: \(currentPage + 1)")
-            }
-            .padding(-14)
-            
-            HStack {
-                
-                Button {
-                    guard currentPage > 0 else {
-                        currentPage = pages.count - 1
-                        direction = .reverse
-                        return
-                    }
-                    direction = .reverse
-                    currentPage -= 1
-                } label: {
-                    Label("next page", systemImage: "chevron.backward")
-                        .foregroundStyle(.white)
-                        .labelStyle(.iconOnly)
-                        .font(.title)
-                        .bold()
-                    
-                }
-                
-                Spacer()
-                
-                Button {
-                    guard currentPage < (pages.count - 1) else {
-                        direction = .forward
-                        currentPage = 0
-                        return
-                    }
-                    direction = .forward
-                    currentPage += 1
-                } label: {
-                    Label("next page", systemImage: "chevron.forward")
-                        .foregroundStyle(.white)
-                        .labelStyle(.iconOnly)
-                        .font(.title)
-                        .bold()
-                    
-                }
-            }
-            .padding(2)
+                PageControl(numberOfPages: pages.count, currentPage: $currentPage)
+                .frame(width: CGFloat(pages.count * 18))
+                .padding(.trailing)
+        
+               // chevrons
+
         }
     }
 }
@@ -67,4 +29,56 @@ struct PageView<Page: View> : View {
 #Preview("Page View") {
     PageView(pages: ModelData().features.map({ FeatureCard(landmark: $0) }))
         .aspectRatio(1.5, contentMode: .fit)
+}
+
+
+
+
+extension PageView {
+    var chevrons: some View {
+                    HStack {
+        
+                        Button {
+                            guard currentPage > 0 else {
+                                currentPage = pages.count - 1
+                                direction = .reverse
+                                return
+                            }
+                            direction = .reverse
+                            currentPage -= 1
+                        } label: {
+                            Label("next page", systemImage: "chevron.backward")
+                                .labelStyle(.iconOnly)
+                                .font(.title)
+                                .bold()
+        
+                        }
+        
+                        Spacer()
+        
+                        Button {
+                            guard currentPage < (pages.count - 1) else {
+                                direction = .forward
+                                withAnimation {
+                                    currentPage = 0
+                                }
+                                return
+                            }
+                            direction = .forward
+                            withAnimation {
+                                currentPage += 1
+                            }
+                        } label: {
+                            Label("next page", systemImage: "chevron.forward")
+        
+                                .labelStyle(.iconOnly)
+                                .font(.title)
+                                .bold()
+        
+                        }
+                    }
+                    .padding(2)
+                    .padding(.vertical, 100)
+                    .foregroundStyle(.blue)
+    }
 }
